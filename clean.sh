c452d769c4bd07e7f7528098b3080919b65bf4b7
Sam Schweigel <s.schweigel@gmail.com>
2019-01-12 17:35:00 -0800

    Comment the scripts and linker script

--- clean.sh
+++ clean.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
+# get the makefile to remove binaries
 if [ -f Makefile ]
 then
     make clean
     rm Makefile
 fi
 
+# delete cmake's mess
 if [ -f CMakeCache.txt ]
 then
     rm -rf CMakeFiles CMakeCache.txt cmake_install.cmake compile_commands.json
 fi
 
+# delete bin
 if [ -d bin ]
 then
     rm -rf bin
