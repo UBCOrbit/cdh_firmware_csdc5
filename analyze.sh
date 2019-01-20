8b8ab4f59dab3c4331e793ccfbee0b21d698c14b
Sam Schweigel <s.schweigel@gmail.com>
2019-01-19 16:14:36 -0800

    Add static analysis script and travis config

---
+++ analyze.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1\
+      -DCMAKE_TOOLCHAIN_FILE=./cmake/static.cmake .
+
+# reformat our source code
+clang-format -style=file -i src/**/*.cpp src/**/*.h src/**/*.c
+
+# run the linter
+clang-tidy src/*
+
+# run the static analyzer
+scan-build make -j4
