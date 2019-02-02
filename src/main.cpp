d356bb0b8a2fc196e9123a909593a8320fddc440
Sam Schweigel <s.schweigel@gmail.com>
2019-02-02 12:26:32 -0800

    Generate three different binaries for each chip

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,19 @@
 #include "hardware_init.h"
 #include "os.h"
 
+#if TRILLIUM==1
+const int led_pin = GPIO_PIN_0;
+#elif TRILLIUM==2
+const int led_pin = GPIO_PIN_7;
+#elif TRILLIUM==3
+const int led_pin = GPIO_PIN_14;
+#endif
+
 int main() {
     clock_init();
     hardware_init();
+
+    HAL_GPIO_WritePin(GPIOB, led_pin, GPIO_PIN_SET);
 
     // Does not return.
     os_init();
