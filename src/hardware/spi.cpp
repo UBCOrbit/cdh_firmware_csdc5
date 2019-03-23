87e8fbab6b4c1828b944e295a53d6f3bc7a51f60
Tristen Raab <tristenraab@gmail.com>
2019-03-23 15:36:09 -0700

    SPI Communication Second Push

--- src/hardware/spi.cpp
+++ src/hardware/spi.cpp
@@ -1,46 +1,158 @@
+#include "hardware/spi.h"
 #include <cstring>
 
-#include "hardware/uart.h"
-
-/**
- * @brief Construct a SPI peripheral.
- *
- * @param port Which SPI peripheral to communicate with. Refer
- * to ST documentation for which SPI can be used on which pins.
- * @param baud Baud rate.  Most of the time, you will want 9600
- * or 115200.
- * @param 
- * @param 
- * @param 
- */
-
-SPI::SPI(SPI::Port, uint32_t baud, GPIO::Port gpio, uint8_t miso, uint8_t mosi, 
-        unit8_t slck, unit8_t nss, uint32_t mode, unit32_t direction, unit32_t nss_mode):
-        port(port),regs(SpiPortregs[port]),baud(baud),
-        miso(), mosi(), slck(), nss(), mode(), direction(), nss_mode(){
-            spi[ports] = this;
+SPI::SPI(SPI::Port port, uint32_t baud, GPIO::Port gpio, uint8_t miso, uint8_t mosi, 
+        uint8_t slck, uint8_t nss, uint32_t mode, uint32_t direction, uint32_t nss_mode):
+        port(port),
+        baud(baud),
+        miso(gpio,miso, GPIO::Mode::AlternatePP, GPIO::Resistor::None,
+             gpio_afs[port]), 
+        mosi(gpio,mosi, GPIO::Mode::AlternatePP, GPIO::Resistor::None,
+             gpio_afs[port]), 
+        slck(gpio,slck, GPIO::Mode::AlternatePP, GPIO::Resistor::None,
+             gpio_afs[port]), 
+        nss(gpio,nss, GPIO::Mode::AlternatePP, GPIO::Resistor::None,
+             gpio_afs[port]){
+        spis[port] = this;
     }
 
 /**
  * @brief Initialize a SPI.  This handles initializing the necessary
  * GPIOs and installing interrupt handlers, too.
  */
+ void SPI::init(){
 
-void SPI::init(){
+    miso.init();
+    mosi.init();
+    slck.init();
+    nss.init();
 
-    handle.Instance = regs;
-    handle.Init = {baud,
-                   UART_WORDLENGTH_8B,
-                   UART_STOPBITS_1,
-                   UART_PARITY_NONE,
-                   UART_MODE_TX_RX,
-                   UART_HWCONTROL_NONE,
-                   UART_OVERSAMPLING_16,
-                   UART_ONEBIT_SAMPLING_DISABLED,
-                   UART_PRESCALER_DIV1,
-                   UART_FIFOMODE_DISABLE,
-                   UART_TXFIFO_THRESHOLD_1_8,
-                   UART_RXFIFO_THRESHOLD_1_8};
-    handle.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+ }
 
+ /**
+ * @brief De-initialize a SPI, and power off its dependencies, too
+ * (GPIOs and interrupt handler).
+ */
+void SPI::deinit() {
+    HAL_SPI_DeInit(&handle);
+    dis_funcs[port]();
+    HAL_NVIC_DisableIRQ(irqns[port]);
+
+    miso.deinit();
+    mosi.deinit();
+    slck.deinit();
+    nss.deinit();
 }
+
+ /**
+ * @brief Transmit a message over the SPI (byte array).
+ *
+ * @param data Pointer to the start of the byte array.
+ * @param len Its length.
+ *
+ * @return A promise that can be blocked on to retrieve the status of
+ * the transmission.
+ */
+Async<SPI::SendStatus> SPI::transmit(uint8_t *data, size_t len) {
+    HAL_SPI_Transmit_IT(&handle, data, len);
+    return sender.promise();
+}
+
+/**
+ * @brief Transmit a C string over the SPI.
+ *
+ * This is just a helper function specifically for null-terminated
+ * strings.  Do NOT use this to send binary data; it will be mangled.
+ *
+ * @param msg The C string to be transmitted.
+ *
+ * @return A promise with the transmission status.
+ */
+Async<SPI::SendStatus> SPI::transmit(char *msg) {
+    size_t len = strlen(msg);
+    uint8_t *data = reinterpret_cast<uint8_t *>(msg);
+    
+    return transmit(data, len);
+}
+
+SPI *SPI::spis[6];           //< Pointers to any SPIs in use, so
+                                //  interrupt handlers can access them.
+SPI_HandleTypeDef *SPI::spi_handles[6]; //< The interrupt callbacks
+                                           //  demand we give them HAL
+                                           //  handles.
+
+//TODO: Finish below
+
+/**
+ * Which alternate function configuration is necessary to use a SPI.
+ * TODO: this is not currently complete, add these as more SPIs are
+ * used.
+ */
+const uint8_t SPI::gpio_afs[] = {
+    0, 0, GPIO_AF7_SPI3, 0, 0, 0,
+};
+
+/**
+ * The interrupt number for each SPI.  We need these to know which to
+ * enable/disable upon initialization.
+ */
+const IRQn_Type SPI::irqns[6] = {
+    SPI1_IRQn, SPI2_IRQn, SPI3_IRQn, SPI4_IRQn,
+    SPI5_IRQn,  SPI6_IRQn, 
+};
+
+/**
+ * Port number to port registers lookup table.  (The HAL wants a register block.)
+ */
+SPI_TypeDef *const SPI::portregs[] = {
+    SPI1, SPI2, SPI3, SPI4, SPI5, SPI6,
+};
+
+/**
+ * Functions to enable SPI clocks by number.
+ */
+void (*const SPI::en_funcs[6])() = {
+    []() { __HAL_RCC_SPI1_CLK_ENABLE(); },
+    []() { __HAL_RCC_SPI2_CLK_ENABLE(); },
+    []() { __HAL_RCC_SPI3_CLK_ENABLE(); },
+    []() { __HAL_RCC_SPI4_CLK_ENABLE(); },
+    []() { __HAL_RCC_SPI5_CLK_ENABLE(); },
+    []() { __HAL_RCC_SPI6_CLK_ENABLE(); },
+};
+
+/**
+ * Functions to disable SPI clocks by number.
+ */
+void (*const SPI::dis_funcs[6])() = {
+    []() { __HAL_RCC_SPI1_CLK_DISABLE(); },
+    []() { __HAL_RCC_SPI2_CLK_DISABLE(); },
+    []() { __HAL_RCC_SPI3_CLK_DISABLE(); },
+    []() { __HAL_RCC_SPI4_CLK_DISABLE(); },
+    []() { __HAL_RCC_SPI5_CLK_DISABLE(); },
+    []() { __HAL_RCC_SPI6_CLK_DISABLE(); },
+};
+
+
+// /**
+//  * @brief Called by the HAL when a nonblocking UART transmission is
+//  * complete.
+//  */
+// void HAL_SPI_TxCpltCallback(void *d, SPI_HandleTypeDef *) {
+//     static_cast<SPI *>(d)->sender.fulfill_isr(SPI::SendStatus::COMPLETE);
+// }
+
+// /**
+//  * @brief Called by the HAL when a nonblocking UART transmission has
+//  * been aborted by @ref UART::abort().
+//  */
+// void HAL_SPI_TxAbortCallback(void *d, SPI_HandleTypeDef *) {
+//     static_cast<SPI *>(d)->sender.fulfill_isr(SPI::SendStatus::ABORTED);
+// }
+
+// /**
+//  * @brief Called by the HAL when a nonblocking UART operation has
+//  * failed.
+//  */
+// void HAL_SPI_ErrorCallback(void *d, SPI_HandleTypeDef *) {
+//     static_cast<SPI *>(d)->sender.fulfill_isr(SPI::SendStatus::ERROR);
+// }
--- src/hardware/spi.h
+++ src/hardware/spi.h
@@ -7,51 +7,78 @@
 #include "os/async.h"
 #include "util/lookup.h"
 
-class SPI : public Hardware{
-    public:
+/*
+ * Need to forward-declare interrupt handlers and callbacks so they
+ * can be declared friends later.
+ */
+//extern "C" void HAL_SPI_TxCpltCallback(void *, SPI_HandleTypeDef *);
+//extern "C" void HAL_SPI_TxAbortCallback(void *, SPI_HandleTypeDef *);
+//extern "C" void HAL_SPI_ErrorCallback(void *, SPI_HandleTypeDef *);
 
-        /**
-         * @brief Type-safe SPI port selection.
-        */
-        enum Port{S1, S2, S3, S4, S5, S6};
+class SPI : public Hardware {
+public:
+    SPI();
 
-        /**
-         * @brief Possible results for a SPI transmission.
-         *
-         * A transmission can either complete successfully, be aborted
-         * (successfully) by the user, or fail because of a hardware
-         * issue.
-         */
-        enum class SendStatus { COMPLETE, ABORTED, ERROR };
+    /**
+     * @brief Type-safe SPI port selection.
+     */
+    enum Port { S1, S2, S3, S4, S5, S6 };
 
-        SPI(Port port, uint32_t baud, GPIO::Port gpio, uint8_t miso, uint8_t mosi, 
-        unit8_t slck, unit8_t nss, uint32_t mode, unit32_t direction, unit32_t nss_mode);
+    /**
+     * @brief Possible results for a SPI transmission.
+     *
+     * A transmission can either complete successfully, be aborted
+     * (successfully) by the user, or fail because of a hardware
+     * issue.
+     */
+    enum class SendStatus { COMPLETE, ABORTED, ERROR };
 
-        void init() override;
-        void deinit() override;
+    SPI(Port port, uint32_t baud, GPIO::Port gpio, uint8_t miso, uint8_t mosi,
+        uint8_t slck, uint8_t nss, uint32_t mode, uint32_t direction,
+        uint32_t nss_mode);
 
-    protected:
+    void init() override;
+    void deinit() override;
 
-        Port port;                 //< The SPI we own.
-        SPI_TypeDef *regs;         //< Pointer to our register block;
-        uint32_t baud;             //< Intended baudrate.
-        SPI_HandleTypeDef handle;  //< HAL Handle for the SPI.
+    Async<SendStatus> transmit(uint8_t *, size_t len);
+    Async<SendStatus> transmit(char *);
 
-        GPIO miso;                 //< GPIO miso pin that we own
-        GPIO mosi;                 //< GPIO mosi pin that we own
-        GPIO slck;                 //< GPIO serial-clock pin that we own
-        GPIO nss;                  //< GPIO NSS pin that we own
+protected:
+    Port port;                //< The SPI we own.
+    SPI_TypeDef *regs;        //< Pointer to our register block;
+    uint32_t baud;            //< Intended baudrate.
+    SPI_HandleTypeDef handle; //< HAL Handle for the SPI.
 
-        Producer<SendStatus> sender;  //< Creates @ref Async instances for us.
+    GPIO miso; //< GPIO miso pin that we own
+    GPIO mosi; //< GPIO mosi pin that we own
+    GPIO slck; //< GPIO serial-clock pin that we own
+    GPIO nss;  //< GPIO NSS pin that we own
 
-        static SPI_TypeDef *const SpiPortregs[8];
-        static const uint8_t gpio_afs[8];
-        static void (*const en_funcs[8])();
-        static void (*const dis_funcs[8])();
-        static const IRQn_Type irqns[8];
+    Producer<SendStatus> sender; //< Creates @ref Async instances for us.
 
-        static SPI *spi[8];
-        static SPI_HandleTypeDef *spi_handles[8];
+    static SPI_TypeDef *const portregs[6];
+    static const uint8_t gpio_afs[6];
+    static void (*const en_funcs[6])();
+    static void (*const dis_funcs[6])();
+    static const IRQn_Type irqns[6];
 
-        // Friend stuff
-}
+    static SPI *spis[6];
+    static SPI_HandleTypeDef *spi_handles[6];
+
+    /*
+     * These handlers and callbacks need to be friends so that they
+     * can access uarts and uart_handles.
+     */
+    //friend void ::HAL_SPI_TxCpltCallback(void *, SPI_HandleTypeDef *);
+    //friend void ::HAL_SPI_TxAbortCallback(void *, SPI_HandleTypeDef *);
+    //friend void ::HAL_SPI_ErrorCallback(void *, SPI_HandleTypeDef *);
+
+    // friend void ::handle_usart1_irq();
+    // friend void ::handle_usart2_irq();
+    // friend void ::handle_usart3_irq();
+    // friend void ::handle_uart4_irq();
+    // friend void ::handle_uart5_irq();
+    // friend void ::handle_usart6_irq();
+    // friend void ::handle_uart7_irq();
+    // friend void ::handle_uart8_irq();
+};
