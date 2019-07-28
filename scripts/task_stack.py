a80c98852ad399485effb82d7cd4d265a6861c97
Sam Schweigel <s.schweigel@gmail.com>
2019-07-27 17:30:05 -0700

    Backport script to python 3.5

--- scripts/task_stack.py
+++ scripts/task_stack.py
@@ -7,26 +7,27 @@ import sys
 from re import findall
 from pathlib import Path
 
-print(f'{"task":10}{"allocated":>15}{"used":>15}')
+print('{:10}{:>15}{:>15}'.format('task', 'allocated', 'used'))
 print('----------------------------------------')
 
 tasks = []
 
 # lmao what is this
 for filename in Path('src').glob('**/*.cpp'):
+    filename = str(filename)
     with open(filename, 'r') as f:
         s = f.read()
         toks = findall(r"[\w']+|[.,!?;]", s)
         for i, t in enumerate(toks):
             if t == 'StaticTask':
                 used = None
-                with open(f'CMakeFiles/csdc5-a.elf.dir/{filename}.su') as suf:
+                with open('CMakeFiles/csdc5-a.elf.dir/{}.su'.format(filename)) as suf:
                     sut = suf.readlines()
                     stoks = map(lambda s: findall(r"[\w\.]+", s), sut)
                     for su in stoks:
                         if toks[i+7] == su[4]:
                             if su[-1] != 'static':
-                                print(f'task {toks[i+2]} does not use a static stack size')
+                                print('task {} does not use a static stack size'.format(toks[i+2]))
                                 exit(1)
                             used = int(su[-2])
                 tasks.append({'name': toks[i+2],
@@ -37,7 +38,7 @@ for filename in Path('src').glob('**/*.cpp'):
 
 failed = False
 for t in tasks:
-    print(f'{t["name"]:10}{t["alloc"]:>15}{t["used"]:>15}')
+    print('{:10}{:>15}{:>15}'.format(t["name"], t["alloc"], t["used"]))
     if (t["alloc"] < t["used"]):
         failed = True
 
